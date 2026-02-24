80247718a51b9662077d971d28e7bfd19dfd4f98
Andro Christian Partahi Sidabutar <andro.sidabutar@diamond.co.id>
2026-02-24 16:54:38 +0700

    ganti config

--- stdpalletingproduct/settings.py
+++ stdpalletingproduct/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -88,11 +90,11 @@ DATABASES = {
 
     'oracle_prod': {
         'ENGINE': 'django.db.backends.oracle',
-        'NAME': 'dbwhs',
-        'USER': 'datagatemonitoring',
-        'PASSWORD': 'datagatemonitoring12345',
-        'HOST': 'dbdatawhs.diamond.co.id',
-        'PORT': '1521',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
 
